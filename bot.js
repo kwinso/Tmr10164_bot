be20ab5475a49732e3f8e8f1a01e1138e780aef3
uwumouse <pythonisajoke@gmail.com>
2020-12-07 22:13:47 +0300

    🧍Теперь используется введенное имя

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ const dropOldUpdatesModern = filter(({ message }) => {
   return !message || message.date > (now - 60 * 2);
 });
 
-const BOT_TOKEN = "<блять ненавижу токены палить сука>"
+const BOT_TOKEN = "1346820594:AAHljIBK2lELio9nN0oFJS2_okce063WzF8"
 
 const bot = new Telegraf(BOT_TOKEN);
 bot.use(dropOldUpdatesModern);
@@ -24,9 +24,10 @@ bot.on("inline_query", async ({ inlineQuery, answerInlineQuery }) => {
     try {
                 
         const programmer = Math.floor(Math.random() * 100) + 1;
+        const query = inlineQuery.query.trim();
         let foundVoices;
-        if (inlineQuery.query.trim()) {
-            foundVoices = voices.filter(v => v.tags.includes(inlineQuery.query.trim()));
+        if (query) {
+            foundVoices = voices.filter(v => v.tags.includes(query));
         } else {
             foundVoices = voices;
         } 
@@ -41,13 +42,13 @@ bot.on("inline_query", async ({ inlineQuery, answerInlineQuery }) => {
         repsonse.unshift({
             type: "article", 
             id: 1,
-            title: `Какой уровень программирования у ${inlineQuery.query || "у тебя"}?`,
+            title: `Какой уровень программирования у ${query || "у тебя"}?`,
             thumb_url: "https://i.ibb.co/3dkm2wk/tmr-pic.jpg",
             reply_markup: Markup.inlineKeyboard([
                 Markup.switchToCurrentChatButton("Поделиться своим уровнем программирования", '')
             ]),
             input_message_content: {
-                message_text: `У меня ${programmer} паяльник(-ов)!`
+                message_text: query ? `У ${query} есть ${programmer} паяльник(-ов)!` : `У меня ${programmer} паяльник(-ов)!`
             }
         })
         return answerInlineQuery(repsonse, { cache_time: 10})
