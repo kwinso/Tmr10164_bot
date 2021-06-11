c194781ca86c46106d7d670ed1eb3cafc11e1570
uwumouse <pythonisajoke@gmail.com>
2021-06-11 20:40:46 +0300

    Added production config with webhooks

--- bot.js
+++ bot.js
@@ -3,26 +3,26 @@ const path = require("path");
 const { Telegraf, filter } = require('telegraf');
 const Markup = require('telegraf/markup');
 
-const dropOldUpdatesModern = filter(({ message }) => {
-  const now = new Date().getTime() / 1000;
-  return !message || message.date > (now - 60 * 2);
+const dropUpdates = filter(({ message }) => {
+    const now = new Date().getTime() / 1000;
+    return !message || message.date > (now - 60 * 2);
 });
 
 require("dotenv").config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.use(dropOldUpdatesModern);
+bot.use(dropUpdates);
 
-let voices; 
+let voices;
 
 async function loadVoices() {
-    const jsonVoices = await fs.readFileSync(path.join(__dirname, "timur.json"));
+    const jsonVoices = fs.readFileSync(path.join(__dirname, "timur.json"));
     voices = JSON.parse(jsonVoices);
-} 
+}
 
 bot.on("inline_query", async ({ inlineQuery, answerInlineQuery }) => {
     try {
-                
+
         const programmer = Math.floor(Math.random() * 100) + 1;
         const query = inlineQuery.query.trim();
         let foundVoices;
@@ -30,17 +30,17 @@ bot.on("inline_query", async ({ inlineQuery, answerInlineQuery }) => {
             foundVoices = voices.filter(v => v.tags.includes(query));
         } else {
             foundVoices = voices;
-        } 
+        }
         const repsonse = foundVoices.map((v, index) => {
             return {
-                type: "voice", 
+                type: "voice",
                 id: index + 2,
                 title: v.title,
                 voice_url: v.voice_url
             }
         });
         repsonse.unshift({
-            type: "article", 
+            type: "article",
             id: 1,
             title: `Какой уровень программирования у ${query || "у тебя"}?`,
             thumb_url: "https://i.ibb.co/3dkm2wk/tmr-pic.jpg",
@@ -51,16 +51,33 @@ bot.on("inline_query", async ({ inlineQuery, answerInlineQuery }) => {
                 message_text: query ? `У ${query} есть ${programmer} паяльник(-ов)!` : `У меня ${programmer} паяльник(-ов)!`
             }
         })
-        return answerInlineQuery(repsonse, { cache_time: 10})
+        return answerInlineQuery(repsonse, { cache_time: 10 })
     } catch (e) {
         console.log(e);
     }
 });
 
+
 async function startBot() {
     await loadVoices();
-    await bot.launch();
-    console.log("Bot started");
+
+    if (process.env.BOT_ENV == "prod") {
+        const port = parseInt(process.env.PORT || "3000");
+
+        await bot.launch({
+            webhook: {
+                domain: process.env.WH_DOMAIN,
+                port
+            }
+        });
+
+        console.log(`Bot started in production configuraion on port ${port}`);
+    }
+    else if (process.env.BOT_ENV == "dev") {
+        await bot.launch();
+        console.log("Bot stated in dev configuraion");
+    }
+    else console.log("Unknown BOT_ENV Value");
 }
 
 startBot();
